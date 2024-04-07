8980039472f86b714a6611c6805c964e7bcb4ab2
Cassius Souza <cassius.sanches@hotmail.com>
2024-04-07 10:03:59 -0300

    add dependencies

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('hello world!')
+/* Dependencies */
+
+const areObjects = require('are-objects');
+const areArrays = require('are-arrays');
+
+
+
+const sum = (a, b) => a + b
+exports.sum = sum
